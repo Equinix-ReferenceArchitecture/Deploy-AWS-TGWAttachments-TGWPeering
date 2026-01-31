421bb4cd2fb84e21269474c3bf3e199881e1aa65
vipin2060 <vipinvikraman86@gmail.com>
2026-01-31 17:20:45 +0800

    Change AWS provider regions to ap-southeast-1 and ap-east-1

--- providers.tf
+++ providers.tf
@@ -15,14 +15,14 @@ terraform {
 
 
 provider "aws" {
-  region     = "us-east-1" 
+  region     = "ap-southeast-1" 
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
 provider "aws" {
-  alias = "us-west-1"
-  region     = "us-west-1"
+  alias = "ap-east-1"
+  region     = "ap-east-1"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
